44a0ae64c85f2d6e256867a50ae71505ef1fed13
Heiker Gil <gilheyker@gmnail.comgasdf>
2021-02-11 21:01:43 -0500

    delete

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 3000
-const Product = require('./models/product')
-
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended:true}))
-
+const port = 3000;
 const mongoose = require('mongoose');
+const Product = require('./models/product')
+const methodOverride = require('method-override')
+
+
+
+
 
 mongoose.connect('mongodb://localhost/mongoAPP', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
@@ -20,12 +20,17 @@ mongoose.connect('mongodb://localhost/mongoAPP', {useNewUrlParser: true, useUnif
 });
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:true}))
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
+
+
+const categories = ['fruit', 'vegetable', 'dairy', 'baked goods'];
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new')
+    res.render('products/new', { categories })
 } )
 
 app.get('/products', async (req, res) => {
@@ -36,14 +41,21 @@ app.get('/products', async (req, res) => {
   app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id)
-    console.log(product)
-    res.render('products/edit', { product })
+    res.render('products/edit', { product, categories })
 })
+
+
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true, useFindAndModify: false })
+    res.redirect(`/products/${product._id}`) 
+})
+
 
   app.get('/products/:id', async (req, res) => {
       const { id } = req.params;
       const product = await Product.findById(id)
-      console.log(product)
       res.render('products/show', { product })
   })
 
@@ -55,4 +67,15 @@ app.get('/products', async (req, res) => {
       res.redirect('products')
 
   })
+
+  app.delete('/products/:id', async (req, res) => {
+      const { id } = req.params;
+      const deletedProduct = await Product.findByIdAndDelete(id);
+      res.redirect('/products');
+  })
   
+
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
